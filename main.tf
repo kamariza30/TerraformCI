63094ad7434f9e72adf53b5626815655159323e1
kamariza30 <kamarizasuhaini@gmail.com>
2025-11-12 20:37:41 +0800

    first commit

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_version = ">=1.3.7"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.43.0"
+    }
+  }
+  cloud {
+
+    organization = "parctice"
+
+    workspaces {
+      name = "TerraformCI"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "1-ebfcadc5-playground-sandbox"
+  location = "South Central US"
+
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "kamastorage102"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "RAGRS"
+
+}
+
